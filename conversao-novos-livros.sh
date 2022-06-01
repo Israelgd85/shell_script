a48598bf902df23989175065f139a3ef5b8b4977
israelgd85 <gomesdeconto@gmail.com>
2022-06-01 17:44:28 -0300

    adicionando função v1

--- conversao-novos-livros.sh
+++ conversao-novos-livros.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-cd /LABS/shell_script/scripts-I/imagens-novos-livros
-for arquivo in *
-do
-	if [ if -d $arquivo ]
-	then
-		cd $arquivo
-		for conteudo_arquivo in *
-		do
-			if [ -d $conteudo_arquivo ]
-			then
-				#Entrar no diretorio e fazer a varredura
-			else
-				#Conversao jpg para png
-			fi
-		done
-	else
-		#Conversao jpg para png
-	fi
-done
-
+varrer_diretorio(){
+	cd $1 
+	for arquivo in *
+	do
+		if [ if -d $arquivo ]
+		then
+			varrer_diretorio $aquivo
+		else
+			#Conversao jpg para png
+		fi
+	done
+}
