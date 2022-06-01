db0183e539c212e5cb998266c6da300998f8c001
israelgd85 <gomesdeconto@gmail.com>
2022-06-01 18:08:02 -0300

    Adicionada a função converte imagem

--- conversao-novos-livros.sh
+++ conversao-novos-livros.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+converte_imagem(){
+	local caminho_imagem=$1
+	local imagem_sem_extensao=$(ls $caminho_imagem | awk -F. '{ print $1}')
+	convert $imgagem_sem_extensao.jpg $imgagem_sem_extensao.png
+}
+
+
 varrer_diretorio(){
 	cd $1 
 	for arquivo in *
@@ -7,9 +14,17 @@ varrer_diretorio(){
 		local caminho_arquivo=$(find /LABS/shell_script/scripts-I/imagens-novos-livros -name $arquivo)
 		if [ if -d $arquivo ]
 		then
-			varrer_diretorio $aquivo
+			varrer_diretorio $caminho_arquivo
 		else
-			#Conversao jpg para png
+			converte_imagem $caminho_arquivo
 		fi
 	done
 }
+
+varrer_diretorio /LABS/shell_script/scripts-I/imagens-novos-livros
+
+if [ $? -eq 0]
+then
+	echo "Conversão realizada com sucesso"
+else
+	echo "Houve um problema na conversão"
