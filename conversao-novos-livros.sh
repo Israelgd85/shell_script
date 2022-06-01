0bfd6eb36642991fe1e63bc7234e6aac62098a77
israelgd85 <gomesdeconto@gmail.com>
2022-06-01 17:53:37 -0300

    adicionando o find

--- conversao-novos-livros.sh
+++ conversao-novos-livros.sh
@@ -4,6 +4,7 @@ varrer_diretorio(){
 	cd $1 
 	for arquivo in *
 	do
+		local caminho_arquivo=$(find /LABS/shell_script/scripts-I/imagens-novos-livros -name $arquivo)
 		if [ if -d $arquivo ]
 		then
 			varrer_diretorio $aquivo
