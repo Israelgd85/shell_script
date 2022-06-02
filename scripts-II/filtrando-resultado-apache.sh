6b74346aa2d5c7b4815e55225203d358641e5ca3
israelgd85 <gomesdeconto@gmail.com>
2022-06-02 03:45:12 -0300

    Adicionada validação do filtro ip, verifica se o ip digita existe no arquivo

--- scripts-II/filtrando-resultado-apache.sh
+++ scripts-II/filtrando-resultado-apache.sh
@@ -3,9 +3,18 @@ cd apache-log/
 
 regex="\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
 
+
+#Valida se o formata do IP digitado pelo usuário esta corrento
 if [[ $1 =~ $regex ]]
 then
 	cat apache.log | grep $1
+	
+	#Verifica se o ip informado existe no arquivo
+	if [ $? -ne 0 ]
+	then
+		echo "O endereço IP procurado não esta presente no arquivo"
+	fi
+
 else
 	echo "Formato não é valido"
 fi
