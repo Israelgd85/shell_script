afe0ada9654169b2533b2b60c86110e8d3e8b90e
israelgd85 <gomesdeconto@gmail.com>
2022-06-03 18:58:02 -0300

    Adicionado filtro para letra maiuscula e identificar se o param esta vazio

--- scripts-II/filtro-requisicao.sh
+++ scripts-II/filtro-requisicao.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 cd /LABS/shell_script/scripts-II/apache-log
 
-case $1 in
+if [ -z $1  ]
+then
+	read -p "Você esqueceu de colocar o parâmentro (GET,POST,PUT,DELETE): " requisicao
+	letra_maiuscula=$(echo $requisicao | awk '{ print toupper($1)}')
+else
+	letra_maiuscula=$(echo $requisicao | awk '{print toupper($1)}')
+fi
+
+case $letra_maiuscula in
 	GET)
 	cat apache.log | grep GET
 	;;
